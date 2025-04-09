a4671a6450ff0588f1b252ba379bdcdd8fce84c2
Salah Eddine Hali <salhali@e2r5p2.1337.ma>
2025-04-09 21:01:03 +0100

    Done

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CC = cc
 
 CFLAGS = -Wall -Wextra -Werror -Lminilibx-linux -lmlx -lXext -lX11 -lm  #-g -fsanitize=address
 
-SRC = get_len/get_next_line.c get_len/get_next_line_utils.c map_parsing.c helper.c so_long.c ft_printf/ft_printf.c ft_printf/ft_putadd.c ft_printf/ft_puthex.c ft_printf/ft_putstr.c ft_printf/ft_putchar.c ft_printf/ft_putnbr.c ft_printf/ft_putunbr.c helper_2.c cleanning.c load_game.c check_parsing.c  helper_3.c check_walls.c cpy_map.c 
+SRC = get_len/get_next_line.c get_len/get_next_line_utils.c map_parsing.c helper.c so_long.c ft_printf/ft_printf.c ft_printf/ft_putadd.c ft_printf/ft_puthex.c ft_printf/ft_putstr.c ft_printf/ft_putchar.c ft_printf/ft_putnbr.c ft_printf/ft_putunbr.c helper_2.c cleanning.c load_game.c check_parsing.c helper_3.c check_walls.c cpy_map.c 
 
 MLX_PATH = ./minilibx-linux/
 
--- so_long.c
+++ so_long.c
@@ -6,77 +6,18 @@
 /*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 16:42:03 by salhali           #+#    #+#             */
-/*   Updated: 2025/04/09 20:54:09 by salhali          ###   ########.fr       */
+/*   Updated: 2025/04/09 20:59:39 by salhali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	init_game_struct(t_game *game)
+void	setup_hooks_and_loop(t_game *game)
 {
-	ft_memset(game, 0, sizeof(t_game));
-}
-
-void	free_map_and_copy(t_game *game)
-{
-	int	i;
-
-	i = 0;
-	while (game->map && game->map[i])
-		free(game->map[i++]);
-	free(game->map);
-	i = 0;
-	while (game->cpy_map && game->cpy_map[i])
-		free(game->cpy_map[i++]);
-	free(game->cpy_map);
-}
-
-int	check_valid_map_and_path(t_game *game)
-{
-	if (!(check_map(game->map)) || !validate_path(game))
-	{
-		free_map_and_copy(game);
-		error("Error: Invalid map path");
-		return (0);
-	}
-	return (1);
-}
-
-int	create_window_and_image(t_game *game, t_data *img, int size)
-{
-	game->mlx_window = mlx_new_window(game->mlx, game->win_x * size, game->win_y
-			* size, "Man7waaa");
-	if (!game->mlx_window)
-	{
-		error("Error: Window creation failed");
-		cleanup(game);
-		return (0);
-	}
-	img->img_window = mlx_new_image(game->mlx, game->win_x * size, game->win_y
-			* size);
-	if (!img->img_window)
-	{
-		error("Error: Image creation failed");
-		cleanup(game);
-		return (0);
-	}
-	img->addr = mlx_get_data_addr(img->img_window, &img->bits_per_pixel,
-			&img->line_length, &img->endian);
-	game->img = img->img_window;
-	return (1);
-}
-
-int	init_graphics(t_game *game, t_data *img)
-{
-	int	size;
-
-	size = 64;
-	game->mlx = mlx_init();
-	if (!game->mlx)
-		exit(EXIT_FAILURE);
-	if (!create_window_and_image(game, img, size))
-		return (0);
-	return (1);
+	mlx_hook(game->mlx_window, 17, 0, close_window, game);
+	mlx_hook(game->mlx_window, KeyPress, KeyPressMask, key_handler, game);
+	mlx_key_hook(game->mlx_window, key_handler, game);
+	mlx_loop(game->mlx);
 }
 
 int	main(int ac, char **av)
